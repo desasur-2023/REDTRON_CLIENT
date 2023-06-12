8c3f9417e008473ecc253113691aabeba2668876
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-06-12 19:26:51 -0300

    empezando el navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
-'use client'
+"use client";
 import Image from "next/image";
-import Logo from "/app/assets/logo.png"
+import Logo from "/app/assets/logo.png";
 
 export default function Home() {
   return (
     <main>
-      <Image
-       
-        src={Logo}
-        alt="REDTRON Logo"
-        width={191}
-        height={182}
-        priority
-      />
+      <Image src={Logo} alt="REDTRON Logo" width={191} height={182} priority />
       <h2>BIENVENIDO!</h2>
       <form action="">
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
 
-        <a href="">Olvide mi Contraseña</a>
+        <a className="align-right" href="">Olvide mi Contraseña</a>
 
         <button>ENTRAR</button>
       </form>
