45ef356e1c312d156e7dfcd0577c67cf4fd23687
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-06-14 17:43:36 -0300

    vista de cajeros ok, falta despues modificar el header para cuando cambian las vistas

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     //           xmlns="http://www.w3.org/2000/svg"
     //           viewBox="0 0 24 24"
     //           fill="currentColor"
-    //           className="w-6 h-6"
+    //           className="w-10 h-10"
     //         >
     //           <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
     //           <path
@@ -55,7 +55,7 @@ export default function Home() {
     //           xmlns="http://www.w3.org/2000/svg"
     //           viewBox="0 0 24 24"
     //           fill="currentColor"
-    //           className="w-6 h-6"
+    //           className="w-10 h-10"
     //         >
     //           <path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />
     //         </svg>
@@ -66,7 +66,7 @@ export default function Home() {
     //           xmlns="http://www.w3.org/2000/svg"
     //           viewBox="0 0 24 24"
     //           fill="currentColor"
-    //           className="w-6 h-6"
+    //           className="w-10 h-10"
     //         >
     //           <path
     //             fillRule="evenodd"
@@ -81,7 +81,7 @@ export default function Home() {
     //           xmlns="http://www.w3.org/2000/svg"
     //           viewBox="0 0 24 24"
     //           fill="currentColor"
-    //           className="w-6 h-6"
+    //           className="w-10 h-10"
     //         >
     //           <path d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875z" />
     //           <path d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 001.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 001.897 1.384C6.809 12.164 9.315 12.75 12 12.75z" />
@@ -107,7 +107,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -126,7 +126,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -145,7 +145,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -164,7 +164,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -183,7 +183,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -202,7 +202,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -221,7 +221,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -240,7 +240,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -259,7 +259,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -278,7 +278,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -297,7 +297,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -316,7 +316,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -335,7 +335,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -354,7 +354,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
@@ -373,7 +373,7 @@ export default function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-6 h-6"
+                className="w-10 h-10"
               >
                 <path
                   fillRule="evenodd"
