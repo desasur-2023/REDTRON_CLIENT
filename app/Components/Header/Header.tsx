d25cba8cf1a58c2ef3717a07468ff5fe93153fb0
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-07-07 23:31:51 -0300

    todo lo mio

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -21,7 +21,7 @@ export default function header({setOpen}) {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-7 h-7"
+          className="w-5 h-5"
         >
           <path
             fillRule="evenodd"
